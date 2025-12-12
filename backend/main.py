3d7cc00820f16a9726ced1cffbddc1edaeef6c6f
Artem <artem.konovalov.2001@inbox.ru>
2025-12-13 00:13:27 +0300

    .env rm

--- backend/main.py
+++
@@ -1,8 +0,0 @@
-import os
-import io
-import pandas as pd
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from minio import Minio
-from minio.error import S3Error
-
-app = FastAPI(title="Cloud Analytics Service")
